04f31eccc473743fe628c169316a243555d2a4ce
Lucantonio <145992078+whats9980@users.noreply.github.com>
2025-11-03 22:29:56 +0000

    Salvar

--- script.js
+++ script.js
@@ -23,5 +23,54 @@ document.addEventListener('DOMContentLoaded', funcion () {
     function validadeForm() {
         const ifEmailValid = validateEmail();
         const isPasswordValid = validatePassword();
+
+        return isEmailValid && isPasswordValid
+    }
+
+    function validateEmail() {
+        const email = emailInput.value.trim();
+        const emailRegex = /^[^\s@^]+@[^\s@]+\.[^\s@]+$/;
+
+        if (email === '') {
+            showError(emailInput, 'E-mail é obrigatório');
+            return false;
+        } else if (!emailRegex.test(email)) {
+            showError(emailInput, 'E-mail inválido');
+            return false;
+        } else {
+            showSucess(emailInput);
+            return true;
+        }
+    }
+
+    function validadePassword() {
+        const password = passwordInput.value.trim();
+
+        if (password === '') {
+            showError(passwordInput, 'Senha é obrigatória');
+            return false;
+        } else if (password.length < 6) {
+            showError(passwordInput, 'Senha deve ter pelo menos 6 caracteres');
+            return false;
+        } else {
+            showSucess(passwordInput);
+            return true;
+        }
+    }
+
+    function showError(input, message) {
+        const inputGroup = input.parentElement;
+
+
+        input.classList.remove('sucess');
+
+
+        input.classList('error');
+
+
+        const existingError = inputGroup.querySelector('.error-message');
+        if (existingError) {
+            existingError.remove();
+        }
     }
 }
